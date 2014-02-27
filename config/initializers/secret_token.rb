164a92944ed929d1694a5abb7a8d169ca8c41257
itzzq <padmazero@gmail.com>
2014-02-27 14:00:07 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Idealink::Application.config.secret_key_base = '4a848381997e4d81e978eae998361930fb9c615d02a1235a89dec241243ab6a802d305defcc82516ef048151f9fe36209c62c24308724e34440b64401dd16a65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Idealink::Application.config.session_store :cookie_store, key: '_idealink_session'
